39a6617a4227677b5362431376fc718eb7808644
Kaloyanov5 <kaloyanovbojidar@gmail.com>
2025-02-12 22:24:40 +0200

    Changed database user login information and added registerUser logic

--- src/main/java/form/user_registration/service/UserService.java
+++ src/main/java/form/user_registration/service/UserService.java
@@ -1,9 +1,11 @@
 package form.user_registration.service;
 
 import form.user_registration.model.RegistrationRequest;
+import form.user_registration.model.User;
 import form.user_registration.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,7 +14,22 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
     public ResponseEntity<?> register(RegistrationRequest request) {
-        return ResponseEntity.ok("test");
+        if (userRepository.findUserByUsername(request.getUsername()).isPresent()) {
+            return ResponseEntity.badRequest().body("Username already exists!");
+        }
+
+        if (!request.getPassword().equals(request.getConfirmPassword())) {
+            return ResponseEntity.badRequest().body("Passwords do not match!");
+        }
+
+        String hashedPassword = passwordEncoder.encode(request.getPassword());
+        User user = new User();
+        user.setUsername(request.getUsername());
+        user.setPassword(hashedPassword);
+        userRepository.save(user);
+        return ResponseEntity.ok("User registered successfully!");
     }
 }
