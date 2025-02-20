cc49b8e4c7711aa859c9c45f63fff6c23224e276
Kaloyanov5 <kaloyanovbojidar@gmail.com>
2025-02-20 09:43:35 +0200

    Added `getStudents` and `deleteStudent` functions

--- src/main/resources/static/scripts.js
+++ src/main/resources/static/scripts.js
@@ -6,6 +6,9 @@ const loginForm = document.getElementById("login-form");
 const registerForm = document.getElementById("register-form");
 const switchToRegisterButton = document.getElementById("switch-to-register");
 const switchToLoginButton = document.getElementById("switch-to-login");
+const dashboard = document.getElementById("dashboard");
+
+let user_id = 0;
 
 loginButton.addEventListener("click", async () => {
     const username = document.getElementById("login-username").value;
@@ -20,7 +23,13 @@ loginButton.addEventListener("click", async () => {
             body: JSON.stringify({ username, password })
         });
 
-        alert(response.ok ? "Login successful" : "Login failed. Please check your credentials.");
+        if (response.ok) {
+            loginForm.style.display = "none";
+            dashboard.style.display = "flex";
+
+            user_id = await response.text();
+            await getStudents(user_id);
+        }
     } catch (error) {
         console.error("Error logging in:", error);
     }
@@ -61,3 +70,54 @@ switchToLoginButton.addEventListener("click", () => {
     registerForm.style.display = "none";
     loginForm.style.display = "flex";
 });
+
+const getStudents = async (id) => {
+    try {
+        const response = await fetch(`${API_BASE_URL}/students/${id}`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+
+        if (response.ok) {
+            const studentTable = document.getElementById("student-table");
+            const students = await response.json();
+            studentTable.innerHTML = students.map(student => `
+                <tr>
+                  <td>${student.id}</td>
+                  <td>${student.fullName}</td>
+                  <td>${student.age}</td>
+                  <td>${student.phoneNumber}</td>
+                  <td>${student.address}</td>
+                  <td><button onclick="deleteStudent(${student.id})">Delete</button></td>
+                </tr>
+            `).join("");
+        }
+    } catch (error) {
+        console.error("Error fetching students:", error);
+    }
+};
+
+const deleteStudent = async (id) => {
+    const promptResponse = confirm("Are you sure you want to delete this student?");
+    if (promptResponse) {
+        try {
+            const response = await fetch(`${API_BASE_URL}/students/${id}`, {
+                method: "DELETE",
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            });
+
+            if (response.ok) {
+                await getStudents(user_id);
+                alert("Student deleted successfully.");
+            } else {
+                alert("Failed deleting student.");
+            }
+        } catch (error) {
+            console.error("Error deleting student:", error);
+        }
+    }
+};
